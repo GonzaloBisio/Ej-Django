b3a7666f7f7476d193a7c339dd9b64d1fb052dbc
GonzaloBisio2002.Gonzalo <GonzaloBisio2002.crackbisio@gmail.com>
2020-06-24 17:45:14 -0300

    Agregando mayor precticidad al utilizar los models

--- biblioteca/models.py
+++ biblioteca/models.py
@@ -1,9 +1,11 @@
 from django.db import models
+from django.contrib import admin
 
 # Create your models here.
 
+
 class Autor(models.Model):
-    nombre = models.CharField(max_length=50)
+    nombre = models.CharField (max_length=50)
     def __str__(self):
         return str (self.nombre)
 
